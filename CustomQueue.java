c2d34202316fd1f4b995a8808c8451f65d71442e
ngngha <51174475+ngngha@users.noreply.github.com>
2022-11-30 16:47:24 +0700

    Add files via upload

---
+++ CustomQueue.java
@@ -0,0 +1,67 @@
+import java.util.ArrayList;
+import java.util.Queue;
+
+public class CustomQueue<T> {
+    private int front= -1;
+    private int rear= -1;
+    private ArrayList<T> queue = new ArrayList<>();
+    //lấy ra ptu đầu hàng đợi
+    public T front(){
+        if(front!=-1){
+            return queue.get(front);
+        }
+        return null;
+    }
+    //lấy ra ptu mới vào hàng đợi
+    public T rear(){
+        if(rear!=-1){
+            return queue.get(rear);
+        }
+        return null;
+    }
+    //thêm ptu vào hàng đợi
+    public void enqueue(T element){
+        if(isEmpty()){
+            rear = 0;
+            front = 0;
+            queue.add(element);
+        }else{
+            front++;
+            queue.add(element);
+        }
+    }
+    //xóa ptu đầu hàng đợi
+    public T dequeue(){
+        if(!isEmpty()){
+            rear++;
+        }
+        return null;
+    }
+
+    public boolean isEmpty(){
+//        boolean response = false;
+        if(front == -1 && rear == -1){
+            return true;
+        }
+        return false;
+    }
+
+    public String toString() {
+        return "queue=" + queue;
+    }
+
+    public static void main(String[] args) {
+        CustomQueue<String> q1 = new CustomQueue<>();
+        q1.enqueue("ha");
+        q1.enqueue("hai");
+        q1.enqueue("hieu");
+        q1.enqueue("duc");
+        q1.enqueue("quy");
+        System.out.println("Queue: "+q1);
+//        q1.remove();
+        System.out.println("Q1's head: "+q1.front());
+        System.out.println("Q1 rear: "+q1.rear());
+    }
+
+}
+
