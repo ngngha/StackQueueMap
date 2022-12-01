d135af1c06bdee5c79c7d412e43f942a1905652d
ngngha <51174475+ngngha@users.noreply.github.com>
2022-12-01 13:22:48 +0700

    Update CustomQueue.java

--- CustomQueue.java
+++ CustomQueue.java
@@ -1,11 +1,9 @@
 import java.util.ArrayList;
 
 public class CustomQueue<T> {
-    private int size = 0;
     private ArrayList<T> queue = new ArrayList<>();
-    
     public boolean isEmpty(){
-        if(size == 0){
+        if(queue.size() == 0){
             return true;
         }
         return false;
@@ -14,44 +12,34 @@ public class CustomQueue<T> {
     public String toString() {
         return "queue=" + queue;
     }
-    //thêm ptu vào hàng đợi
-     public void add(T element) {
+
+    public void add(T element) {
         queue.add(element);
     }
-    //xóa ptu đầu hàng đợi
+
     public T remove() {
-        T value = null;
-        size = queue.size();
+        T x;
+        x = queue.get(0);
         if (isEmpty()) {
             return null;
         }else{
-            value = queue.get(0);
             queue.remove(0);
+            return x;
         }
-        return value;
     }
-    //lấy ra ptu đầu hàng đợi
-    public T getFirst() {
-        size = queue.size();
+
+    public T front() {
         if (isEmpty()){
             return null;
         }
         return queue.get(0);
     }
 
-    public int size() {
-        return queue.size();
+    public T rear() {
+        if (isEmpty()){
+            return null;
+        }
+        return queue.get(queue.size()-1);
     }
-
-    public static void main(String[] args) {
-        CustomQueue<Integer> q2 = new CustomQueue<>();
-        q2.add(1);
-        q2.add(2);
-        System.out.println(q2);
-        System.out.println(q2.getFirst());
-        q2.remove();
-        System.out.println(q2);
-        System.out.println(q2.getFirst());
-    }
+    
 }
-
