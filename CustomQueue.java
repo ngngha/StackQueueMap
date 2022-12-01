7d4476981dec57ec1aaf7fe3ebb4f88ef4d23c23
ngngha <51174475+ngngha@users.noreply.github.com>
2022-12-01 11:17:06 +0700

    Update CustomQueue.java

--- CustomQueue.java
+++ CustomQueue.java
@@ -1,53 +1,11 @@
 import java.util.ArrayList;
 
 public class CustomQueue<T> {
-    private int front= -1;
-    private int rear= -1;
+    private int size = 0;
     private ArrayList<T> queue = new ArrayList<>();
-    //lấy ra ptu đầu hàng đợi
-    public T front(){
-        if(front!=-1){
-            return queue.get(front);
-        }
-        return null;
-    }
-    //lấy ra ptu mới vào hàng đợi
-    public T rear(){
-        if(rear!=-1){
-            return queue.get(rear);
-        }
-        return null;
-    }
-    //thêm ptu vào hàng đợi
-    public void enqueue(T element){
-        if(isEmpty()){
-            rear = 0;
-            front = 0;
-            queue.add(element);
-        }else{
-            front++;
-            queue.add(element);
-        }
-    }
-    //xóa ptu đầu hàng đợi
-    public T dequeue(){
-        T x;
-        x = queue.get(front);
-        if(isEmpty()){
-            return null;
-        }else if(front == rear){
-            front = rear = -1;
-            queue.remove(x);
-            return x;
-        }else{
-            queue.remove(x);
-            rear++;
-            return x;
-        }
-    }
-
+    
     public boolean isEmpty(){
-        if(front == -1 && rear == -1){
+        if(size == 0){
             return true;
         }
         return false;
@@ -56,18 +14,46 @@ public class CustomQueue<T> {
     public String toString() {
         return "queue=" + queue;
     }
+    //thêm ptu vào hàng đợi
+     public void add(T element) {
+        queue.add(element);
+    }
+    //xóa ptu đầu hàng đợi
+    public T remove() {
+        T value = null;
+        size = queue.size();
+        if (isEmpty()) {
+            return null;
+        }else{
+            value = queue.get(0);
+            queue.remove(0);
+        }
+        return value;
+    }
+    //lấy ra ptu đầu hàng đợi
+    public T getFirst() {
+        size = queue.size();
+        if (isEmpty()){
+            return null;
+        }
+        return queue.get(0);
+    }
+
+    public int size() {
+        return queue.size();
+    }
 
     public static void main(String[] args) {
-        CustomQueue<String> q1 = new CustomQueue<>();
-        q1.enqueue("ha");
-        q1.enqueue("hai");
-        q1.enqueue("hieu");
-        q1.enqueue("duc");
-        q1.enqueue("quy");
-        System.out.println("Queue: "+q1);
-//        q1.remove();
-        System.out.println("Q1's head: "+q1.front());
-        System.out.println("Q1 rear: "+q1.rear());
+        CustomQueue<Integer> q2 = new CustomQueue<>();
+        q2.add(1);
+        q2.add(2);
+        System.out.println(q2);
+        System.out.println(q2.getFirst());
+        System.out.println(q2.getLast());
+        q2.remove();
+        System.out.println(q2);
+        System.out.println(q2.getFirst());
+        System.out.println(q2.getLast());
     }
 
 }
