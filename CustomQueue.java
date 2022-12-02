56c37c41f21275e78ce8ada7352a68b9fb867b58
ngngha <51174475+ngngha@users.noreply.github.com>
2022-12-02 15:57:09 +0700

    Add files via upload

--- CustomQueue.java
+++ CustomQueue.java
@@ -1,45 +1,77 @@
-import java.util.ArrayList;
-
 public class CustomQueue<T> {
-    private ArrayList<T> queue = new ArrayList<>();
-    public boolean isEmpty(){
-        if(queue.size() == 0){
-            return true;
+
+    private class Node{
+        T data;
+        Node next;
+
+        public T getData() {
+            return data;
         }
-        return false;
+
+        public void setData(T data) {
+            this.data = data;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+
+        public Node() {
+        }
+
+    }
+    private Node head;
+
+    public CustomQueue() {
+    }
+    public boolean isEmpty() {
+        return head == null;
     }
 
-    public String toString() {
-        return "queue=" + queue;
-    }
-    //thêm ptu vào hàng đợi
-    public void add(T element) {
-        queue.add(element);
-    }
-    //xóa đi ptu đầu hàng đợi
-    public T remove() {
-        T x;
-        x = queue.get(0);
+    public void add(T element){
+        Node x = new Node();
         if (isEmpty()) {
+            head = x;
+            x.setData(element);
+        }else {
+            Node tempNode = head;
+            while (tempNode.getNext()!=null){
+                tempNode = tempNode.getNext();
+            }
+                tempNode.setNext(x);
+                x.setData(element);
+        }
+    }
+    public Node remove(){
+        Node x;
+        if(isEmpty()){
             return null;
-        }else{
-            queue.remove(0);
+        }else {
+            x=head;
+            head = x.getNext();
             return x;
         }
     }
-    //lấy ra ptu đầu ngăn xếp nhưng không xóa
-    public T getFirst() {
+
+    public T getHead() {
         if (isEmpty()){
             return null;
         }
-        return queue.get(0);
+        return head.getData();
     }
 
-    public T getLast() {
-        if (isEmpty()){
-            return null;
-        }
-        return queue.get(queue.size()-1);
+    public static void main(String[] args) {
+        CustomQueue<Integer> q2 = new CustomQueue<>();
+        q2.add(7);
+        q2.add(8);
+        q2.add(9);
+        q2.add(10);
+        q2.remove();
+        System.out.println(q2.getHead());
+
     }
-    
 }
